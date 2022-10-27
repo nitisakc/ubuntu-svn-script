5ecd431b5f204fa2c75f82201b3cef8856ce019a
GuillenDiego <104097818+GuillenDiego@users.noreply.github.com>
2022-10-27 16:47:55 -0500

    Update setupSVN.sh
    This solves the copying file problem.

--- setupSVN.sh
+++ setupSVN.sh
@@ -15,7 +15,7 @@ sudo a2enmod dav
 sudo a2enmod dav_svn
 sudo service apache2 restart
 
-sudo cp $HOME/ubuntu-svn-script/dav_svn.conf /etc/apache2/mods-enabled/dav_svn.conf
+sudo cp ubuntu-svn-script/dav_svn.conf /etc/apache2/mods-enabled/dav_svn.conf
 
 sudo mkdir $SVN_DIR
 sudo svnadmin create ${SVN_DIR}/repository
